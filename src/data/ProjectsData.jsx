6726b9414080eea6bffc4f409b10916a39d7392f
Antonyo Shamuel <shamuelmota@gmail.com>
2026-04-17 16:40:13 -0300

    feat: Correção de link - Correção do link do projeto portifolio

--- src/data/ProjectsData.jsx
+++ src/data/ProjectsData.jsx
@@ -8,7 +8,7 @@ export const PROJECTS = [
     "tags": ["React", "CSS", "JavaScript", "HTML"],
     "type": TYPE,
     "img": reactIcon,
-    "link": "",
+    "link": "https://github.com/shmota/portifolio-app",
     "description": `Projeto de portifólio pessoal, desenvolvido para apresentar minhas habilidades técnicas e projetos realizados. 
     O portifólio é responsivo e apresenta um design moderno e clean, com seções dedicadas para habilidades, projetos e informações pessoais.`
   }
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
+  base: '/portifolio-app/',
   plugins: [
     react(),
     tailwindcss(),
